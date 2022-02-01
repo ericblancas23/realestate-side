c31aa5310ec21203af8b205d620bc08c3781665c
Eric <ericblancas23@gmail.com>
2022-02-01 09:27:44 -0800

    init

---
+++ hooks/useFetchApi.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export const baseUrl = "https://bayut.p.rapidapi.com";
+
+export const useFetchApi = async (url) => {
+  const { data } = await axios.get(url, {
+    headers: {
+      "x-rapidapi-host": "bayut.p.rapidapi.com",
+      "x-rapidapi-key": "958f0911f0msh3f8d3362c603605p13d0e1jsn3a86f86fbf63",
+    },
+  });
+  return data;
+};
